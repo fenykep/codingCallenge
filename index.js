f019df9b4eb8a7fedd08da9c9418265a1f4cd5bc
Ábel <fenykep@Fodor-MacBook-Pro-2.local>
2022-09-12 22:51:50 +0200

    all tasks in funcs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//function substitutes data with a local copy if I'm coding on the metro
+//function substitutes data with a local copy if I'm coding on the tram
 require('dns').resolve('challenge.codingsans.com', function(err) {
   if (err) {
   	console.log("Cant connect, using local copy.")
@@ -41,103 +41,39 @@ const godFatherDesire =[
   }];
 
 async function printData(data){
-	var recipes = alpSort(data.recipes);
 
 	// solution for task 1
-	//console.log("Last week's sales: " + getWeeklySales(data) + " Ft");
+	// console.log("Last week's sales: " + getWeeklySales(data.salesOfLastWeek, data.recipes) + " Ft");
 	//---------------------------------------------------------------
 
 	// solution for task 2
-	console.log(altMenus(data.recipes));
+	// console.log(altMenus(data.recipes));
 	//---------------------------------------------------------------
 	
-	var ingPriceList = ingPriceLister(data.wholesalePrices);
-	var profPerItem = recipeProfitLister(data.recipes, ingPriceList);
+	// var ingPriceList = ingPriceLister(data.wholesalePrices);
+	// var profPerItem = recipeProfitLister(data.recipes, ingPriceList);
 
 	// solution for task 3
-	// console.log(getWeeklyProfit(data, profPerItem));
+	// console.log(getWeeklyProfit(data.salesOfLastWeek, profPerItem));
+	console.log(getWeeklyProfit(data.salesOfLastWeek, data.wholesalePrices, data.recipes));
 	//---------------------------------------------------------------
 
 	// solution for task 4
-	// var useInvUp = [];
-	// for (const r of recipes) {
-	// 	useInvUp.push(useOneInvUp(r,data.inventory));
-	// }
-	// console.log(JSON.stringify(useInvUp, null, 3));
+	// console.log(JSON.stringify(useAllInvUp(data.recipes, data.inventory), null, 2));
 	//---------------------------------------------------------------	
 
-	// solution for task 5
-	var losses = 0;
-	var nedAmnt = 0;
-	// for(const gfd of godFatherDesire){
-	// 	var ingList = findKey(recipes, "name", gfd.name, "ingredients");
-	// 	console.log(">ereceptbű kell " + gfd.amount);
-	// 	for(const ing of ingList){
-	// 		var bulksize = ing.amount.split(' ')[1]=='pc' ? 1:1000;
-	// 		losses += findKey(data.wholesalePrices, "name", ing.name, "price") * Math.ceil(uSrp(ing.amount)*gfd.amount / uSrp(findKey(data.wholesalePrices, "name", ing.name, "amount"))/bulksize);
-	// 	}
-	// }
-	// console.log("Palikám bukott " + losses + " forintot. Sad :( ");
+	// solution for task 5	
+	// console.log('This  "order" will set us back ' + costOfCustomOrder(godFatherDesire, data.recipes, data.wholesalePrices) + " forints. Sad :( ");
 	//---------------------------------------------------------------	
 
 	// solution for task 6
-	// 
-	//		console.log(inPol7(data, 2, 0.1));
-
-	var orderList = [];
-	for(const reci of data.salesOfLastWeek){
-	
-		var weeklySold = reci.amount;
-		var ingList = findKey(data.recipes, "name", reci.name, "ingredients");
-
-		for(const ing of ingList){
-			var bulksize = ing.amount.split(' ')[1]=='pc' ? 1:1000;
-			reqAmount = uSrp(ing.amount) / bulksize * 2.2 * weeklySold;
-			// reqAmount = Math.round(reqAmount*10000)/10000;
-			var bulkOrderIndex = objInList(orderList, "name", ing.name);
-			// if the ingredient is already in the list of ings to bulkorder,
-			// then increase orderamount,
-			// else ad to the list
-			if (bulkOrderIndex!=false) {
-				orderList[bulkOrderIndex-1] = {
-					"name": ing.name,
-					"amount": Number(orderList[bulkOrderIndex-1].amount) + reqAmount,
-					"totalPrice": 0
-				}
-			}
-			else{
-				orderList.push({
-					"name": ing.name,
-					"amount": reqAmount,
-					"totalPrice": 0
-				})
-			}
-		}
-	}
-
-	for(const [index,ing] of orderList.entries()){
-		ingsubstract = uSrp(findKey(data.inventory, "name", ing.name, "amount"));
-		orderList[index].amount = ing.amount - ingsubstract;
-		if (orderList[index].amount <= 0) {
-			orderList.splice(index,1);
-		}
-
-	}
-
-	for(const [index, ing] of orderList.entries()){
-		divDer = findKey(data.wholesalePrices, "name", ing.name, "amount").split(' ');
-		palettNo = Math.ceil(ing.amount / divDer[0]);
-		price = palettNo * findKey(data.wholesalePrices, "name", ing.name, "price");
-		orderList[index].amount = (palettNo * divDer[0]) + " " +  divDer[1];
-		orderList[index].totalPrice = price;
-
-	}
-
-	// console.log(JSON.stringify(orderList, null, 2));
-
+	// console.log(JSON.stringify(prep4future(data.salesOfLastWeek, data.recipes, data.inventory, data.wholesalePrices, 2, 0.1), null, 2));
+	//---------------------------------------------------------------	
 }
 
-function inPol7(salesOfLastweek, recipes, inventory, wolesalePrices, weeks, overead){
+//calculates ingredients to be ordered to last [weeks] based on [salesOfLastWeek]
+//with a safety [overhead], with prices and amounts based off [wholesalePrices]
+function prep4future(salesOfLastWeek, recipes, inventory, wholesalePrices, weeks, overhead){
 	var orderList = [];
 	for(const reci of salesOfLastWeek){
 		var weeklySold = reci.amount;
@@ -145,7 +81,7 @@ function inPol7(salesOfLastweek, recipes, inventory, wolesalePrices, weeks, over
 
 		for(const ing of ingList){
 			var bulksize = ing.amount.split(' ')[1]=='pc' ? 1:1000;
-			reqAmount = uSrp(ing.amount) / bulksize * weeks * (overead+1) * weeklySold;
+			reqAmount = uSrp(ing.amount) / bulksize * weeks * (overhead+1) * weeklySold;
 			var bulkOrderIndex = objInList(orderList, "name", ing.name);
 			// if the ingredient is already in the list of ings to bulkorder,
 			// then increase orderamount,
@@ -180,10 +116,23 @@ function inPol7(salesOfLastweek, recipes, inventory, wolesalePrices, weeks, over
 	for(const [index, ing] of orderList.entries()){
 		divDer = findKey(wholesalePrices, "name", ing.name, "amount").split(' ');
 		palettNo = Math.ceil(ing.amount / divDer[0]);
-		price = palettNo * findKey(data.wholesalePrices, "name", ing.name, "price");
+		price = palettNo * findKey(wholesalePrices, "name", ing.name, "price");
 		orderList[index].amount = (palettNo * divDer[0]) + " " +  divDer[1];
 		orderList[index].totalPrice = price;
 	}
+
+	//just trying out reordering the bulkOrderList according to the
+	//wholesale list
+	var newOrderList = [];
+	for(const ing of wholesalePrices){
+		var pushy = findKey(orderList, "name", ing.name);
+		console.log(pushy);
+		if(pushy){
+			newOrderList.push(pushy);
+		}
+	}
+
+	return orderList;
 }
 
 function objInList(list, key, keyVal){
@@ -197,12 +146,10 @@ function objInList(list, key, keyVal){
 }
 
 //alphabetically sorts arrays of objects by their [name]
-function alpSort(array){
-	array = array.sort((a,b)=> {
-		var a1 = a.name.toLowerCase();
-		var b1 = b.name.toLowerCase();
-		return a1<b1 ?-1:a1> b1? 1 :0;
-	})
+function alpSort(array, key){
+	array.sort(function(a, b) {
+	  return Intl.Collator().compare(a[key], b[key]);
+	});
 	return array;
 }
 
@@ -229,25 +176,36 @@ function findKey(array, key, keyVal, retVal){
 }
 
 //returns total sales of last week (task 1.)
-function getWeeklySales(data){
+function getWeeklySales(salesOfLastWeek, recipes){
 	var weeklySum = 0;
-	data.salesOfLastWeek.forEach(item => {
-		var uPrice = uSrp(findKey(data.recipes,"name",item.name,"price"));
+	salesOfLastWeek.forEach(item => {
+		var uPrice = uSrp(findKey(recipes,"name",item.name,"price"));
 		// console.log(item.name + " " + uPrice + " Ft * " + item.amount+ " db  = " + uPrice*item.amount + " Ft");
 		weeklySum += uPrice*item.amount;
 	});
 	return weeklySum;
 }
 
-function getWeeklyProfit(data, profPerItem){
+//calculates the profit made from given weeks sales and the precalculated profit made
+//selling each item
+// function getWeeklyProfit(salesOfLastWeek, profPerItem){
+function getWeeklyProfit(salesOfLastWeek, wholesalePrices, recipes){
+	// var ingPriceList = ingPriceLister(wholesalePrices);
+	// var profPerItem = recipeProfitLister(recipes, ingPriceLister(wholesalePrices));
+	// console.log(ingPriceList);
+	// console.log(profPerItem);
+	// console.log(wholesalePrices);
+	// var ingPriceList = ingPriceLister(wholesalePrices);
+	var profPerItem = recipeProfitLister(recipes, ingPriceLister(wholesalePrices));
+
 	var weeklyProfit = 0;
-	data.salesOfLastWeek.forEach(item => {
+	salesOfLastWeek.forEach(item => {
 		weeklyProfit += item.amount * profPerItem[item.name];
 	})
 	return weeklyProfit;
 }
 
-//return
+//returns how many of the given item we can produce with given inventory
 function useOneInvUp(recipe, inv) {
 	//quasi smallest denominator
 	var smllDenom;
@@ -263,6 +221,17 @@ function useOneInvUp(recipe, inv) {
 	retObj.name = recipe.name;
 	retObj.amount = smllDenom;
 	return retObj;
+}
+
+//function to calculate how many of each item on the menu we can produce if we were
+//to use our whole inventory up for all the recipes one by one
+//(so all the flour and sugar for x, then all the initial flour and sugar for y)
+function useAllInvUp(recipes, inventory){
+	var useInvUp = [];
+	for (const r of recipes) {
+		useInvUp.push(useOneInvUp(r,inventory));
+	}
+	return alpSort(useInvUp, "name");
 }
 
 //generates inclusive menus for dietary-restricted people
@@ -309,6 +278,19 @@ function recipeProfitLister(recipes, ingPriceList){
 		recProfList[r.name] = r.price.split(' ')[0] - prodPriceCalc(r, ingPriceList);
 	}
 	return recProfList;
+}
+
+function costOfCustomOrder(order, recipes, wholesalePrices){
+	var losses = 0;
+	var nedAmnt = 0;
+	for(const gfd of order){
+		var ingList = findKey(recipes, "name", gfd.name, "ingredients");
+		for(const ing of ingList){
+			var bulksize = ing.amount.split(' ')[1]=='pc' ? 1:1000;
+			losses += findKey(wholesalePrices, "name", ing.name, "price") * Math.ceil(uSrp(ing.amount)*gfd.amount / uSrp(findKey(wholesalePrices, "name", ing.name, "amount"))/bulksize);
+		}
+	}
+	return losses;
 }
 
 //generates an object where keys are ingredients and values are price per smaller unit
